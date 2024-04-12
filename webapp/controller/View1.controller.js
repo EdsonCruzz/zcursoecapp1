1667812348590bb2555a4740ba958ef0eff27d10
Edson <edsonmoraiscruz@gmail.com>
2024-04-11 22:12:21 -0300

    Terceira aula

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,6 +10,41 @@ sap.ui.define([
         return Controller.extend("zcursoecapp1.controller.View1", {
             onInit: function () {
 
+            },
+
+            onCreate: function() {
+                var that = this;
+                var usuario     = this.getView().byId("idUsuario").getValue();
+                var nome        = this.getView().byId("idNome").getValue();
+                var projsegw    = this.getView().byId("idProjSegw").getValue();
+                var email       = this.getView().byId("idEmail").getValue();
+
+                if (!usuario) {
+                    sap.m.MessageBox.error(this.getView().getModel("i18n").getResourceBundle().getText("lblMsgErrorUser"));
+                    return;
+                }
+
+                var oDados = {
+                    Usuario: usuario,
+                    Nome: nome,
+                    ProjetoSegw: projsegw,
+                    Email: email
+                }
+
+                this.getView().getModel().create('/AlunosFioriEDSONCRSet', oDados, {
+                    success: function (oData, oResponse) {
+                        sap.m.MessageBox.success(that.getView().getModel("i18n").getResourceBundle().getText("lblMsgCreateOK"));
+                        that.getView().byId("idUsuario").setValue("");
+                        that.getView().byId("idNome") .setValue("");
+                        that.getView().byId("idProjSegw").setValue("");
+                        that.getView().byId("idEmail").setValue("");
+                    },
+                    error: function (oError){
+                        sap.m.MessageBox.error(that.getView().getModel("i18n").getResourceBundle().getText("lblMsgCreateError"));
+                    }
+                });
+
             }
+
         });
     });
